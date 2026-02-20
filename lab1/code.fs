11e96c25aaf582db96999e43bf7765a5f2bf0d21
deusdesuu <erkomy.2@mail.ru>
2026-02-20 10:48:48 +0500

    Create code.fs

---
+++ lab1/code.fs
@@ -0,0 +1,44 @@
+open System
+(*
+//Сформировать список из названий дней недели
+[<EntryPoint>]
+let main argv =
+    let week = ["monday"; "tuesday"; "wednesday"; "thursday"; "friday"; "saturday"; "sunday"]
+    for day in week do
+        printf "%s  " day
+    0
+*)
+//Возвратить true, если в числе есть цифра k, и false — в противном случае
+let rec CheckNum num k: bool =
+    if ((num % 10 = k)) then
+        true
+    elif (num / 10 = 0) then
+        false
+    else
+        CheckNum (num / 10) k
+
+let InputHandle : int*int =
+    printf "Введите целое число n: "
+    let num = int(Console.ReadLine())
+    printf "Введите цифру k: "
+    let k = int(Console.ReadLine())
+    
+    let num =
+        if num < 0 then
+            -num
+        else
+            num
+    
+    if ((k < 0) || (k > 9)) then
+        raise (System.ArgumentException("k должна быть цифрой от 1 до 9!!!"))
+    
+    (num, k)
+
+[<EntryPoint>]
+let main argv =
+    let num, k = InputHandle
+    if (CheckNum num k) then
+        printfn "Цифра k есть в данном числе!"
+    else
+        printfn "Цифры k нет в данном числе"
+    0
