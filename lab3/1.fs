b4f2210911f53a72b377539d64c098972811a04d
deusdesuu <erkomy.2@mail.ru>
2026-03-13 10:13:10 +0500

    Update 1.fs

--- lab3/1.fs
+++ lab3/1.fs
@@ -13,59 +13,63 @@
 open System
 
 
-let rec InputNatural(msg) : int =
+let rec inputNatural(msg) : int =
     printf "%s" msg
     try
         let num = int(Console.ReadLine())
         if num <= 0 then
                 printfn "Число должно быть натуральным!!!\n"
-                InputNatural(msg)
+                inputNatural(msg)
         else
             num
     with
         |exn ->
             printfn "Число должно быть натуральным!!!\n"
-            InputNatural(msg)
+            inputNatural(msg)
 
-let rec InputBinNum(msg:string) : string =
+let rec inputBinNum(msg:string) : string =
     printf "%s" msg
     let str = Console.ReadLine()
     
     if not (str |> Seq.forall (fun c -> c = '0' || c = '1')) then
         printf "%s не является бинарным числом\n" str
-        InputBinNum(msg)
+        inputBinNum(msg)
     else
         if (0 < Convert.ToInt32(str, 2) && 
             Convert.ToInt32(str, 2) < 10) then
                 str.TrimStart('0')
         else
             printf "%s не является цифрой 1-9\n" str
-            InputBinNum(msg)
+            inputBinNum(msg)
 
-let Input() : seq<string> =
-    let n = InputNatural("Введите натуральное число n: ")
+let input() : seq<string> =
+    let n = inputNatural("Введите натуральное число n: ")
     seq {
         for i in 1..n do
-            yield InputBinNum(sprintf 
+            yield inputBinNum(sprintf 
             "%d/%d Введите цифру 1..9 в двоичном виде: " i n)
     }
 
 [<EntryPoint>]
 let main _ =
-    let sequence = Input()
+    let sequence = input()
 
-    Seq.map (fun s -> Convert.ToInt32(s, 2)) sequence
-       |> Seq.toList
-       |> printfn "Множество после Seq.map: %A"
+    Seq.map (fun s -> printf "%s " s; Convert.ToInt32(s, 2)) sequence
+    |> Seq.iter (printfn "-> %A ")
     0
 
 (*
 Test:
 Введите натуральное число n: 5
 1/5 Введите цифру 1..9 в двоичном виде: 1
+1 -> 1 
 2/5 Введите цифру 1..9 в двоичном виде: 10
+10 -> 2 
 3/5 Введите цифру 1..9 в двоичном виде: 100
+100 -> 4 
 4/5 Введите цифру 1..9 в двоичном виде: 1000
+1000 -> 8
 5/5 Введите цифру 1..9 в двоичном виде: 101
-Множество после Seq.map: [1; 2; 4; 8; 5]
+101 -> 5
+
 *)
